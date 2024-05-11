87d407ec43231bffa49e0f8d3ed202b67c53fdeb
Anhelina <krukanhelina06@gmail.com>
2024-05-11 14:21:44 +0200

    added --error flag

--- skrypt.sh
+++ skrypt.sh
@@ -22,9 +22,24 @@ elif [ "$1" = "--help" ] || [ "$1" = "-h" ]; then
     echo "  --date : Wyświetla dzisiejszą datę."
     echo "  --logs [liczba_plików] : Tworzy pliki log. Domyślna ilośc to 100."
     echo "  --help : Wyświetla wszystkie dostępne opcje."
-elif [ "$1" = "--init" ]; then
+# skrypt.sh --init
+elif [ "$1" = "--init" ] || [ "$1" = "-i" ] ; then
     git clone git@github.com:anhelinakruk/Lab04.git repository
     repo_path="$(pwd)/repository"
     export PATH="$PATH:$repo_path"
     echo "Repositorium sklonowane i dodane do PATH."
+# skrytp.sh --error
+elif [ "$1" = "--error" ] || [ "$1" = "-e" ]; then
+    if [ -n "$2" ]; then
+        num_errors=$2
+    else
+        num_errors=100
+    fi
+    for ((i=1; i<=num_errors; i++)); do
+        mkdir -p "error$i"
+        filename="error${i}/error${i}.txt"
+        content="Nazwa pliku: $filename\nUtworzono przez: skrypt.sh\nData: $(date +"%Y-%m-%d %H:%M:%S")\n"
+        echo -e "$content" > "$filename"
+    done
+    echo "Utworzono $num_errors plików error w osobnych folderach."
 fi
