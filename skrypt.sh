50ef2ea85d49b094681146addf578ab07ec27d31
Anhelina <krukanhelina06@gmail.com>
2024-05-11 13:22:35 +0200

    add --logs argument

--- skrypt.sh
+++ skrypt.sh
@@ -7,10 +7,15 @@ fi
 
 # skrypt.sh --logs
 if [ "$1" = "--logs" ]; then
-    for ((i=1; i<=100; i++)); do
+    if [ -n "$2" ]; then
+        num_logs=$2
+    else
+        num_logs=100
+    fi
+    for ((i=1; i<=num_logs; i++)); do
         filename="log${i}.txt"
         content="Nazwa pliku: $filename\nUtworzono przez: skrypt.sh\nData: $(date +"%Y-%m-%d %H:%M:%S")\n"
         echo -e "$content" > "$filename"
     done
-    echo "Utworzono 100 plików log."
+    echo "Utworzono $num_logs plików log."
 fi
