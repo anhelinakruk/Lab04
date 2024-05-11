799d84e1e111a529293f9c4aef4fc0d6a167b9d7
Anhelina <krukanhelina06@gmail.com>
2024-05-11 13:12:23 +0200

    add --logs flag

--- skrypt.sh
+++ skrypt.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# skrypt.sh --date
 if [ "$1" = "--date" ]; then
     date +"Dzisiejsza data: %Y-%m-%d"
+fi
+
+# skrypt.sh --logs
+if [ "$1" = "--logs" ]; then
+    for ((i=1; i<=100; i++)); do
+        filename="log${i}.txt"
+        content="Nazwa pliku: $filename\nUtworzono przez: skrypt.sh\nData: $(date +"%Y-%m-%d %H:%M:%S")\n"
+        echo -e "$content" > "$filename"
+    done
+    echo "Utworzono 100 plików log."
 fi
